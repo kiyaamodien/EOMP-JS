04f70fd810862d3f424127ef7cfb0184c8d7c2a4
kiyaamodien <kiyaamudienkhan@gmail.com>
2021-08-13 16:15:30 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-fetch("https:/https://boiling-savannah-92766.herokuapp.com/products/")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    takeaways(data);
-  });
+fetch("https://boiling-savannah-92766.herokuapp.com/show-products");
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        renderProducts(data)
+    })
 
-function takeaways(product) {
-  let product_container = document.querySelector(".product-container");
-  product_container.innerHTML = "";
-  product.data.forEach((car) => {
-    product_container.innerHTML += `
-    <div class = "vehicle">
-        <img src="${product.image_url}" class="vehicle-image size1">
-        <img src="${product.logo}" class="vehicle-logo size2">
-        <h4 class="product_name"> ${product.name}</h4>
-        <p class="product_type"> ${product.type}</p>
-        <p class="price">R${price} </p>
-        <p class="product_quamtity">${product.quantity}</p>
+function renderProducts(products) {
+    let productContainer = document.querySelector("#products-container");
+    productContainer.innerHTML ="";
+    products.forEach(product => {   
+        productContainer.innerHTML += `
+            <div class="product">
+                <img src="${ product.image }" class="product-image">
+                <div class="product-content">
+                    <h4 class="product_name">${ product_name }</h4>
+                    <p class="product_type">${ product_type }</p>
+                    <p class="price">R${ price }</p>
+                    <p class="product_quantity">${ product_quantity }</p>
+                </div>
+            </div>
+        `
+    })
 
-    </div>
-    `;
-  });
 }
